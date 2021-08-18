0cd2d66898f66fdd708329bc827bf5d01cb1eb17
L410N3L <57776908+L410N3L@users.noreply.github.com>
2021-08-17 19:28:19 -0500

    reorganization

--- src/components/Presentational.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Presentacional = () => <h1>quihubo puta</h1>;
-
-export default Presentacional
--- src/components/Stateless.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Stateless = () => {
-    return(
-        <h1>Quihubo puta</h1>
-    );
-};
-
-export default Stateless
